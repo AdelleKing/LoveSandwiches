37f10a2c3c6ea41d0dc782fb14375decff185503
AdelleKing <adelle_xx@yahoo.co.uk>
2023-12-11 14:18:34 +0000

    added functionality to enable stock reccomendations for the next market

--- run.py
+++ run.py
@@ -119,12 +119,32 @@ def get_last_5_entries_sales():
     sales = SHEET.worksheet('sales')
     
     columns =[]
-    for ind in range(1, 6):
+    for ind in range(1, 7):
         column = sales.col_values(ind) #col_values is a built in method from gspread.
     #this returns the column data, accessing the 3rd column in the spreadsheet
     #counting the column index starts from 1 not 0.
         columns.append(column[-5:])
     return columns
+
+
+def calculate_stock_data(data):
+    """
+    Calculates the average stock for each item type, adding 10%
+    """
+
+    print('Calculating stock data...\n')
+
+    new_stock_data =[]
+    for column in data:
+        int_column =[int(num) for num in column]
+        average = sum(int_column) / len(int_column) #using the len() gives functionality
+        #if we don't know how long the list is.
+
+        stock_num = average *1.1
+        new_stock_data.append(round(stock_num))
+    return new_stock_data
+
+
 
 def main():
     """moved all function calls into it's own function
@@ -135,8 +155,9 @@ def main():
     update_worksheet(sales_data, 'sales') #add int data to worksheet
     new_surplus_data = calculate_surplus_data(sales_data) 
     update_worksheet(new_surplus_data, 'surplus')
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, 'stock')
 
 print('Welcome to Love Sandwiches Data\n')
-#main()
-sales_columns = get_last_5_entries_sales()
-print(sales_columns)
+main()
