35231f1f7ab159a29f3d27122797932190a06634
AdelleKing <adelle_xx@yahoo.co.uk>
2023-12-11 12:39:49 +0000

    added functionality to iterate through stock and sales data and return the surplus data.

--- run.py
+++ run.py
@@ -78,8 +78,15 @@ def calculate_surplus_data(sales_row):
 
     print('Calculating surplus data...\n')
     stock = SHEET.worksheet("stock").get_all_values()
-    stock_row = stock[-1]
-    print(stock_row)
+    stock_row = stock[-1] # requests the last line in the list - slice method
+    
+    surplus_data =[]
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+          
 
 
 
@@ -91,7 +98,8 @@ def main():
     data = get_sales_data()#returned data from user input
     sales_data = [int(num) for num in data] # convert returned data into int
     update_sales_worksheet(sales_data) #add int data to worksheet
-    calculate_surplus_data(sales_data) 
+    new_surplus_data = calculate_surplus_data(sales_data) 
+    print(new_surplus_data)
 
 print('Welcome to Love Sandwiches Data\n')
 main()
